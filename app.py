83a65e39fdf6f76bdca04a26340a1bcfe039cfa8
Magic Man <65839659+Magician357@users.noreply.github.com>
2024-05-14 09:03:55 -0700

    convert to fastapi

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
-from flask import Flask, request
+# from flask import Flask, request
+from fastapi import FastAPI, Response, status
 import json
 import hashlib
 # from time import time
 from datetime import datetime
 
-app = Flask(__name__)
+app = FastAPI()
 
 password = "1ea36d6cf235d71982e8a63706bce9f446eccc4027821321be869cb9c79d0cacb8f3f6cf65a4434f25d755bae2e8465d"
 
@@ -23,51 +24,51 @@ def log(text):
 def check_password(text):
     return encrypt(text) == password
 
-@app.route('/')
+@app.get('/')
 def home():
     with open("database.json","r") as f: data=f.read()
-    return f"this is a database. to add data, send a post request to the /set page with the key, value, and password. to get data, send a get request to the /get page with the key. to delete data, send a delete request to /del with the key and password.    {data}"
+    return f"this is a database. to add data, send a post request to the /set page with the key, value, and password in the query. to get data, send a get request to the /get page with the key in the query. to delete data, send a delete request to /del with the key and password in the query. eg getting a key would be sending a get request to http://127.0.0.1:8000/get?key=hello   {data}"
 
-@app.route("/set",methods=["POST","GET"])
-def set():
-    if request.method == "POST":
-        if check_password(request.form['password']):
-            write_json(request.form['key'],request.form['value'])
-            log(f"Data addition: ({request.form['key']}:{request.form['value']})")
-            return {"status":200,"data_recieved":(request.form['key'],request.form['value'])}
-        else:
-            return {"status":401,"data_recieved":(request.form['key'],request.form['value'])}
+@app.post("/set", status_code=200)
+def set(key: str, value:str, password: str, response: Response):
+    if check_password(password):
+        write_json(key,value)
+        log(f"Data addition: ({key}:{value})")
+        return {"status":200,"data_recieved":(key,value)}
     else:
-        return {"status":400,"data_recieved":()}
+        response.status_code = status.HTTP_401_UNAUTHORIZED
+        return {"status":401,"data_recieved":(key,value)}
 
-@app.route("/get",methods=["GET"])
-def get():
+@app.get("/get", status_code=200)
+def get(key:str,response: Response):
     # if check_password(request.form['password']):
-    key=request.form['key']
     with open("database.json","r") as f:
         data=json.load(f)
     if key not in data:
+        response.status_code = status.HTTP_400_BAD_REQUEST
         return {"status":400,"key":key,"value":""}
     # log(f"Data got:")
-    return {"status":200,"key":key,"value":data[request.form['key']]}
+    return {"status":200,"key":key,"value":data[key]}
 
-@app.route("/del",methods=["DELETE","GET"])
-def delete():
-    if check_password(request.form['password']):
-        status=200
+@app.delete("/del", status_code=200)
+def delete(key:str,password:str,response: Response):
+    if check_password(password):
+        # status=200
+        response.status_code = status.HTTP_200_OK
         with open("database.json",'r') as file:
-            key =request.form['key']
             file_data = json.load(file)
             if key not in file_data: 
-                status=400
+                response.status_code = status.HTTP_400_BAD_REQUEST
+                # status=400
             else: 
                 log(f"Data deleted: ({key}:{file_data[key]})")
                 file_data.pop(key)
         with open("database.json","w") as file:
             file.seek(0)
             json.dump(file_data, file, indent = 4)
-        return {"status":status}
+        return {"status":response.status_code}
     else:
+        response.status_code = status.HTTP_401_UNAUTHORIZED
         return {"status":401}
 
 @app.route("/encrypt/<data>")
@@ -76,5 +77,5 @@ def encrypt(data: str):
     hash_object.update(data.encode())
     return hash_object.hexdigest()
 
-if __name__ == "__main__":
-    app.run(debug=True)
+# if __name__ == "__main__":
+    # app.run(debug=True)
